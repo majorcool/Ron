aaa418189d1b6a64c5ba56afe24d55ddf9581f3f
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cnjinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-13 11:00:23 +0800

    1.2.2

--- U1-L1/1.2.2.py
+++ U1-L1/1.2.2.py
@@ -1,6 +1,5 @@
 anwser1 = 1 * 2 + 3 - 4 / 4
 anwser1 += 5
-print(anwser1)
 
 answer2 = 1
 answer2 -= 3
@@ -8,4 +7,4 @@ answer2 *= 2
 answer2 /= 2
 answer2 **= 2
 answer2 //= 2
-print(answer2)
+print("第一种%d,第二种%d"%(anwser1,answer2))
