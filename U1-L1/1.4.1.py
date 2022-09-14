867f410e7703283c02e51af5a4294997f453e848
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cnjinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-14 16:28:58 +0800

    1.4.1

---
+++ U1-L1/1.4.1.py
@@ -0,0 +1,7 @@
+# 选择电脑软件飞书
+send_to_class = "老师考试内容都忘了"  # 飞书发送信息
+nBook = "添加......"  # 聊天区上方加号，添加公告等
+studentName = "张景焕"  # 学习者姓名
+ChooseTheClass = "Python 程序设计"  # 选择课程教室
+cWork = "视频会议"  # 选择功能
+finding = "设计"  # 搜索
