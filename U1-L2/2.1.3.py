51f766b1b70ebe98b2266e75d8f92e8a8ca4e4c8
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cnjinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-15 17:14:56 +0800

    2.1.3

---
+++ U1-L2/2.1.3.py
@@ -0,0 +1,23 @@
+alien_color = "red"
+if alien_color == "red":
+    print("You got 5 points!")
+elif alien_color == "green":
+    print("you get 10 points!")
+else:
+    print("You get 15 points!")
+
+alien_color = "green"
+if alien_color == "red":
+    print("You got 5 points!")
+elif alien_color == "green":
+    print("you get 10 points!")
+else:
+    print("You get 15 points!")
+
+alien_color = "yellow"
+if alien_color == "red":
+    print("You got 5 points!")
+elif alien_color == "green":
+    print("you get 10 points!")
+else:
+    print("You get 15 points!")
