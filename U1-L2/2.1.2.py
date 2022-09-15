10d7789b1ab705e34e1e8bf01c0484e6b81e14bf
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cnjinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-15 17:09:02 +0800

    2.1.2

---
+++ U1-L2/2.1.2.py
@@ -0,0 +1,11 @@
+alien_color = "green"
+if alien_color == "green":
+    print("You got 10 points!")
+else:
+    print("You shot an alien, you get 5 points!")
+
+alien_color = "yellow"
+if alien_color == "green":
+    print("You got 10 points!")
+else:
+    print("You shot this alien, you get 5 points!")
