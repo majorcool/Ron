5c5d06d900a03793b77e5ec99ff69efbc3bb26c9
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-29 11:10:16 +0800

    add

--- U1-L5/5.1.3.py
+++ U1-L5/5.1.3.py
@@ -1,12 +1,10 @@
 # 5.1.3 依次删除下方列表中所有的 3，并在删除后打印每个 3 的索引，最后打印最终的列表
 # list_num = [1, 3, 4, 3, 7, 3, 9, 8, 6, 3]
 list_num = [1, 3, 4, 3, 7, 3, 9, 8, 6, 3]
-del list_num[1]
-print(1)
-del list_num[2]
-print(2)
-del list_num[3]
-print(3)
-del list_num[6]
-print(6)
-print(list_num)
+list_num_new = []
+for num in range(0,len(list_num)):
+    if list_num[num] == 3:
+        print(num)
+    else:
+        list_num_new.append(list_num[num])
+print(list_num_new)
