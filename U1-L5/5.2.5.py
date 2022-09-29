61a896b0f94ac7bfb80463d31ebd55136d8675fc
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-09-29 10:50:09 +0800

    add

--- U1-L5/5.2.5.py
+++ U1-L5/5.2.5.py
@@ -12,3 +12,11 @@
 '''
 
 #不会
+for num in range(1,37):
+    if num <= 9:
+        print("",num, " ", end="")
+    if num > 9:
+        print(num," ",end="")
+    if num == 6 or num == 12 or num == 18 or num == 24 or num == 30 or num == 36:
+        print()
+        #doge
