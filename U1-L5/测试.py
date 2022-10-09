5a012128ea7408ea95a5a98f34c087b54bf8c495
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-09 20:26:39 +0800

    add

--- U1-L5/测试.py
+++ U1-L5/测试.py
@@ -1,7 +1,7 @@
-for num in range(1,37):
-    if num <= 9:
-        print("",num, " ", end="")
-    if num > 9:
-        print(num," ",end="")
-    if num == 6 or num == 12 or num == 18 or num == 24 or num == 30 or num == 36:
-        print()
+# for num in range(1,37):
+#     if num <= 9:
+#         print("",num, " ", end="")
+#     if num > 9:
+#         print(num," ",end="")
+#     if num == 6 or num == 12 or num == 18 or num == 24 or num == 30 or num == 36:
+#         print()
