0345ded7d684343933542717d91c632a15de68e7
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-10 18:01:07 +0800

    add

---
+++ U1-L5/5.3.1.py
@@ -0,0 +1,4 @@
+# 5.3.1 使用一个字典来存储一个熟人的信息，包括名、姓、年龄和居住的城市。该字典应包含键 first_name、last_name、age 和 city 。
+# 将存储在该字典中的每项信息都打印出来
+information = {"first_name": "仔", "last_name": "虚", "age": 20, "city": "YanTai"}
+print(information)
