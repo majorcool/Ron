d5678fd2f5417f1350ad8996d2e5cfc632b85e4c
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-11 08:37:00 +0800

    add

--- U1-L5/5.1.5.py
+++ U1-L5/5.1.5.py
@@ -1,13 +1,19 @@
 # 5.1.5 定义一个函数，作用是将列表中的数字元素全部扩大两倍，其他类型保持不变，修改完成后，将新列表作为返回值
-list_num = [66,"S",1]
-def xk():
-    c = len(list_num)
-    b = 0
-    while b < c:
-        if isinstance(list_num[b], int):
-            num = list_num[b] * 2
-            list_num[b] = num
-        b += 1
+
+
+def xk(list_num):
+    long = len(list_num)
+    number = 0
+    while number < long:
+        if type(list_num[number]) == int:
+            num = list_num[number] * 2
+            list_num[number] = num
+        if type(list_num[number]) == float:
+            num = list_num[number] * 2
+            list_num[number] = num
+        number += 1
     return list_num
-n = xk()
-print(n)
+
+
+n = [66, "S", 1.2]
+print(xk(n))
