88c402a3102bd6cab25393d9cb0ab0ba02ff9c5a
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-11 09:34:39 +0800

    H

--- U1-L2/2.2.3.py
+++ U1-L2/2.2.3.py
@@ -1,15 +1,15 @@
 your_age = int(input("他的年龄："))
 if 1 > your_age >= 0:
     print("他是婴儿")
-elif 2 >= your_age >= 1:
+elif 3 > your_age >= 1:
     print("他是幼儿")
-elif 12 >= your_age >= 3:
+elif 13 > your_age >= 3:
     print("他是儿童")
-elif 16 >= your_age >= 13:
+elif 17 > your_age >= 13:
     print("他正处于青春期")
-elif 24 >= your_age >= 17:
+if 24 >= your_age >= 17:
     print("他是年轻人")
-if 64 >= your_age >= 18:
+if 65 > your_age >= 18:
     print("他是成年人")
 elif your_age >= 65:
     print("他是老人")
