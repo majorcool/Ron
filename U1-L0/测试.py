bc34c7895ccc716e2a481900b2bac58e9bec0b02
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-12 13:00:52 +0800

    6

--- U1-L0/测试.py
+++ U1-L0/测试.py
@@ -1,14 +1,1 @@
-place_1 = " "
-place_2 = " "
-place_3 = " "
-place_4 = " "
-place_5 = " "
-place_6 = " "
-place_7 = " "
-place_8 = " "
-place_9 = " "
-"---- ----- ----"
-"---- ----- ----"
-
-print("Game---Tic-Tac-Toe")
-print("  %s |  %s  | %s \n  %s |  %s  | %s \n  %s |  %s  | %s " % (place_1, place_2, place_3, place_4, place_5, place_6, place_7, place_8, place_9))
+s = "1,2,3,4,5,6,7,8,9,0"
