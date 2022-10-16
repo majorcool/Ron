c845ac268a06a084bad46849cc6c7a209fd8a423
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-16 18:43:39 +0800

    PP

--- PA/PA---Tic-Tac-Toe.py
+++ PA/PA---Tic-Tac-Toe.py
@@ -1,32 +1,54 @@
-place_1 = " "
-place_2 = " "
-place_3 = " "
-place_4 = " "
-place_5 = " "
-place_6 = " "
-place_7 = " "
-place_8 = " "
-place_9 = " "
-
-
-def chess_frame():
+chess_all_place = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+cut_num = []
+step = 1
+while True:
     print("Game---Tic-Tac-Toe")
-    print("  %s |  %s  | %s " % (place_1, place_2, place_3))
+    print("  %s |  %s  | %s " % (chess_all_place[0], chess_all_place[1], chess_all_place[2]))
     print("---- ----- ----")
-    print("  %s |  %s  | %s " % (place_4, place_5, place_6))
+    print("  %s |  %s  | %s " % (chess_all_place[3], chess_all_place[4], chess_all_place[5]))
     print("---- ----- ----")
-    print("  %s |  %s  | %s " % (place_7, place_8, place_9))
+    print("  %s |  %s  | %s " % (chess_all_place[6], chess_all_place[7], chess_all_place[8]))
     print("Please put the chess in the right place, from 1---9")
+    if step % 2 == 1:
+        chess_place = int(input("Player1,X:"))
+        if chess_all_place[chess_place - 1] == "X" or chess_all_place[chess_place - 1] == "O":
+            print("XXXXXXChess's places can't be same")
+            continue
+        chess_all_place[chess_place - 1] = "X"
+        cut_num.append(chess_place - 1)
+        step += 1
+    elif step % 2 == 0:
+        chess_place = int(input("Player2,O:"))
+        if chess_all_place[chess_place - 1] == "X" or chess_all_place[chess_place - 1] == "O":
+            print("XXXXXXChess's places can't be same")
+            continue
+        chess_all_place[chess_place - 1] = "O"
+        cut_num.append(chess_place - 1)
+        step += 1
+    line_1 = chess_all_place[0:3:1]
+    line_2 = chess_all_place[3:6:1]
+    line_3 = chess_all_place[6:9:1]
+    rowe_1 = chess_all_place[0:7:3]
+    rowe_2 = chess_all_place[1:8:3]
+    rowe_3 = chess_all_place[2:9:3]
+    diagonal_1 = chess_all_place[0:9:4]
+    diagonal_2 = chess_all_place[2:7:2]
+    win_X = ["X", "X", "X"]
+    win_O = ["O", "O", "O"]
+    if line_1 == win_X or line_2 == win_X or line_3 == win_X or rowe_1 == win_X or rowe_2 == win_X or rowe_3 == win_X or diagonal_1 == win_X or diagonal_2 == win_X:
+        print("XXXXXXPlayer1 win!!!")
+        break
+    if line_1 == win_O or line_2 == win_O or line_3 == win_O or rowe_1 == win_O or rowe_2 == win_O or rowe_3 == win_O or diagonal_1 == win_O or diagonal_2 == win_O:
+        print("XXXXXXPlayer2 win!!!")
+        break
+    if len(cut_num) == 6:
+        cut_near = int(cut_num[0]) + 1
+        print("XXXXXXafter next step, %d place's chess will be cut" % cut_near)
+    if len(cut_num) == 7:
+        cut_near = int(cut_num[0]) + 2
+        print("XXXXXXafter next step, %d place's chess will be cut" % cut_near)
+    if len(cut_num) > 6:
+        cut = int(cut_num[0])
+        chess_all_place[cut] = " "
+        del cut_num[0]
 
-
-def chess_play():
-    player = 1
-    chess_all_place = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    if player % 2 == 1:
-        place_chess = int(input("Please input the chess place1-9："))
-        chess_all_place[place_chess-1] = "X"
-
-
-
-chess_frame()
-chess_play()
--- U1-L0/测试.py
+++ U1-L0/测试.py
@@ -1,2 +1,36 @@
-str="123456789"
-print(str[-1:-1:-1])
+chess_all_place = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+line_1 = chess_all_place[0:3:1]
+line_2 = chess_all_place[3:6:1]
+line_3 = chess_all_place[6:9:1]
+rowe_1 = chess_all_place[0:7:3]
+rowe_2 = chess_all_place[1:8:3]
+rowe_3 = chess_all_place[2:9:3]
+diagonal_1 = chess_all_place[0:9:4]
+diagonal_2 = chess_all_place[2:7:2]
+win_X = ["X", "X", "X"]
+win_O = ["O", "O", "O"]
+if line_1 == win_O:
+    print(4)
+if line_2 == win_O:
+    print(5)
+if line_3 == win_O:
+    print(6)
+if rowe_1 == win_O:
+    print("1")
+if rowe_2 == win_O:
+    print("2")
+if rowe_1 == win_O:
+    print("3")
+if diagonal_1 == win_O:
+    print(7)
+if diagonal_2 == win_O:
+    print(8)
+print(line_1)
+print(line_2)
+print(line_3)
+print(rowe_1)
+print(rowe_2)
+print(rowe_3)
+print(diagonal_1)
+print(diagonal_2)
