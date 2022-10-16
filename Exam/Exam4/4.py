dde28ebb7fbd77c106a489ea1364d5d3999f611d
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-16 16:16:35 +0800

    4

--- Exam/Exam4/4.py
+++ Exam/Exam4/4.py
@@ -1,13 +1,2 @@
 def add(num1, num2):
-    def different(n, str="123456789"):
-        long = len(str)
-        num = ""
-        num_0 = str
-        for time in range(0, long, n):
-            num_0 = num_0[time:time + n]
-            num_0 = num_0[-1:-len(num_0) - 1:-1]
-            num += num_0
-            num_0 = str
-        return num
-
-    print(different(3))
+    #没想出来
