74444db83028911d429e8b958c48340222291ff2
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-25 09:22:41 +0800

    1

---
+++ U2-L1/10.1.1.py
@@ -0,0 +1,9 @@
+'面向对象'  # Object Oriented Programming, OOP
+'面向过程'  # procedure-oriented
+'类'  # class
+'对象'  # object
+'属性'  # property
+'方法'  # method
+'实例'  # living example
+'实例化'  # instantiation
+
