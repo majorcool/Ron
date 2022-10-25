f6910d66fb81c900b28f8567536a879584677586
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-10-25 09:39:58 +0800

    1

---
+++ U2-L1/10.1.4.py
@@ -0,0 +1,12 @@
+# 10.1.4  定义水果类
+# 定义一个水果类，创建苹果对象、橘子对象、西瓜对象，并分别添加上颜色属性
+class Fruits:
+    pass
+
+
+apple = Fruits()
+orange = Fruits()
+watermelon = Fruits()
+apple.color = "red"
+orange.color = "yellow"
+watermelon.color = "green"
