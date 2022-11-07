2639fd97f81157f3995d281185b13146293c1e71
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-11-07 16:43:11 +0800

    1

--- U2-L2/CC.py
+++ U2-L2/CC.py
@@ -3,24 +3,18 @@
 方法是写在类里的函数
 共同点都有参数，返回值，将一些特定的操作封装在一起
 不同点方法是在类里面的函数，并且方法有一个额外的参数如self
-2内置函数是Python自带的
-内置方法是
-print, input, len, str, int, list, tuple...
-__str__, __len__, __del__, __init__
-3self参数是对象（实例）的引用
-为什么要有self为了给实例和对象进行引用
 
-1打印属性
-Noun enrolled! (id: 0)
 2
-
+内置函数是Python自带的有括号，装参数
+    print,input
+内置方法是封装在类里的内置函数
+    str, int, list, tuple, del, bool
+    __str__, __len__, __del__, __init__
+公共方法是不同类型的变量通用的方法
+    切片
 3
+self参数是对象（实例）的引用
+为什么要有self：为了给实例和对象进行引用
 
-4
 
-
-5
-
-6
-1
 """
