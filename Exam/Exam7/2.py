07d8fb6c40c6febb00b815e6f5306b58082a2c29
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-11-14 18:42:19 +0800

    1

--- Exam/Exam7/2.py
+++ Exam/Exam7/2.py
@@ -2,28 +2,37 @@ class Animal:
     def __init__(self):
         self.health = 100
 
+    def feed_0(self):
+        self.health = self.health + 20
+        return self.health
+
+    def perform_0(self):
+        self.health = self.health - 20
+        return self.health
+
 
 class Manager:
     def __init__(self, performance):
         self.performance = performance
 
-    def feed(self, health: Animal):
-        self.health += 20
-        print("喂")
-
-    def perform(self, health: Animal):
-        self.health -= 20
-        print("演")
-
     def __inspect(self, health: Animal):
-        if self.health < 80:
+        if health < 80:
             self.performance -= 20
-            print("扣")
+            if self.performance < 0:
+                return self.performance == 0
+            else:
+                return self.performance
+
+    def feed(self):
+        return Animal.feed_0(self)
+
+    def perform(self):
+        return Animal.perform_0(self)
 
 
 class Keeper(Manager):
     def __init__(self):
-        super().__init__()
+        super().__init__(self)
 
     def perform(self):
         pass
@@ -31,20 +40,20 @@ class Keeper(Manager):
 
 class Trainer(Manager):
     def __init__(self):
-        super().__init__()
+        super().__init__(self)
 
     def feed(self):
         pass
 
 
 def everyday():
-    manager_0 = Manager(100)
     keeper_0 = Keeper()
     trainer_0 = Trainer()
+    manager_0 = Manager(100)
     pig = Animal()
     keeper_0.feed()
     trainer_0.perform()
-    manager_0._Manager__inspect
+    manager_0._Manager__inspect()
 
 
 everyday()
