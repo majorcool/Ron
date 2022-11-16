e597d8e64312f33da1496f16ab8a2bffc3edc758
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-11-16 10:47:12 +0800

    1

--- U2-L5/14.1.3.py
+++ U2-L5/14.1.3.py
@@ -2,26 +2,30 @@
 # Point 类包含 2 个属性，分别为点的横坐标与纵坐标
 # Segment 类包含 2 个私有属性，分别为两个端点的坐标
 # Segment 类包含 get_len() 方法，可以获得直线的长度
-
-# 这是借鉴的，我还是不会（悲）
-import math
-
-
-class Points:
+class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return str((self.x, self.y))
+
 
 class Segment:
-    def __init__(self, xy1, xy2):
-        self.__xy1 = str(xy1).split(',')
-        self.__xy2 = str(xy2).split(',')
+    def __init__(self, x1, y1, x2, y2):
+        self.__start = Point(x1, y1)
+        self.__end = Point(x2, y2)
 
     def get_len(self):
-        final = math.sqrt(
-            (((int(self.__xy1[0])) - int(self.__xy2[0]))) ** 2 + (((int(self.__xy1[1])) - int(self.__xy2[1]))) ** 2)
-        print(final)
+        x1, y1 = self.__start.x, self.__start.y
+        x2, y2 = self.__end.x, self.__end.y
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-a=Segment('2,5','8,9')
-a.get_len()
+
+a = Point(1, 3)
+b = Point(2, 4)
+c = Segment(1, 3, 2, 4)
+print(c.get_len())
+
+
+
