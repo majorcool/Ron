65f649a7fc57de2a5a1f28f0de20a4a1220c57c3
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-11-28 17:08:04 +0800

    1

--- Exam/Exam9/2.5.py
+++ Exam/Exam9/2.5.py
@@ -3,4 +3,4 @@
 # You are given a 0-indexed integer array amount of length 3 where amount[0], amount[1], and amount[2] denote the number of cold, warm, and hot water cups you need to fill respectively.
 # Return the minimum number of seconds needed to fill up all the cups.
 def rate(brackets: list[list[int]], income: int):
-
+    pass
