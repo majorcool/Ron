40d504bca8eef9e87eaf2d48b2e11bbccfc0725c
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2022-12-20 08:49:47 +0800

    1

---
+++ PA/3/modules.py
@@ -0,0 +1,23 @@
+# color, display, draw, event, font, image, key, locals, mixer, mouse, rect, surface, time, music, pygame
+import pygame
+import sys  # sys 是 python 的标准库，提供 Python 运行时环境变量的操控
+
+
+pygame.init()
+size = width, height = 800, 600  # 窗口大小
+screen = pygame.display.set_mode(size)  # 初始化窗口
+pygame.display.set_caption('game')  # 窗口类型，左上角显示
+
+hello = pygame.font.SysFont("Times New Roman", 100)
+hello_1 = hello.render("color", True, (251, 128, 222))
+screen.blit(hello_1, (0, 0))
+
+pygame.draw.rect(screen, (55, 25, 255), (752, 500, 4, 20), 1)  # 屏幕; 颜色; 位置坐标+长与宽; 是否填充0填充，其他不填充
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
+
+
