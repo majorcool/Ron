013a28464cacc519ffe3d94de486a868112d8f4e
Ron <jinghuan.zhang.2201@stu.moonshotacademy.cn>
2023-01-01 16:46:30 +0800

    1

--- PA/3/dinosaur.py
+++ PA/3/dinosaur.py
@@ -1,9 +1,23 @@
-class Dinosaur:
-    def __init__(self):
-        pass
+import pygame
+
+
+class Dinosaur(pygame.sprite.Sprite):
+    def __init__(self, images, position=(40, 147), size=[(44, 47), (59, 47)], **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images = images
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.bottom = position
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.refresh_rate = 5
+        self.refresh_counter = 0
+
+        self.speed = 10
 
     def jump(self):
-        pass
+        pygame.mixer.Sound('resources/audios/jump.wav').play()
 
     def duck(self):
         pass
@@ -14,11 +28,18 @@ class Dinosaur:
     def die(self):
         pass
 
-    def draw(self):
-        pass
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
-    def load_image(self):
-        pass
+    def refresh(self):  # load_image
+        if self.image == self.images[0]:
+            self.image = self.images[1]
+        else:
+            self.image = self.images[0]
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        pass
+        if self.refresh_counter == self.refresh_rate:
+            self.refresh()
+            self.refresh_counter = 0
+        self.refresh_counter += 1
--- PA/3/main.py
+++ PA/3/main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import random
 from scene import Ground, Cloud
+from obstacle import Cactus, Pterodactyl
 
 FPS = 60
 TITLE = "Chrome Dino"
@@ -10,6 +11,16 @@ SCREENSIZE = (700, 300)
 IMAGE_PATHS = {
     "ground": "resources/images/ground.png",
     "cloud": "resources/images/cloud.png",
+    "cactus1": "resources/images/Cactus1.png",
+    "cactus2": "resources/images/Cactus2.png",
+    "cactus3": "resources/images/Cactus3.png",
+    "cactus4": "resources/images/Cactus4.png",
+    "cactus5": "resources/images/Cactus5.png",
+    "cactus6": "resources/images/Cactus6.png",
+    "dinosaur1": "resources/images/dinosaur1.png",
+    "dinosaur2": "resources/images/dinosaur2.png",
+    "pterodactyl1": "resources/images/pterodactyl1.png",
+    "pterodactyl2": "resources/images/pterodactyl2.png",
 }
 pygame.init()
 screen = pygame.display.set_mode(SCREENSIZE)
@@ -20,6 +31,20 @@ ground = Ground(image_ground, (0, SCREENSIZE[1]))
 
 image_cloud = pygame.image.load(IMAGE_PATHS['cloud'])
 cloud_sprites_group = pygame.sprite.Group()
+
+image_cactus = []
+for i in range(1, 7):
+    __ = "cactus".replace("cactus", "cactus" + str(i))
+    _ = pygame.image.load(IMAGE_PATHS[__])
+    image_cactus.append(_)
+cacti = pygame.sprite.Group()
+
+image_pterodactyl = []
+for i in range(1, 3):
+    __ = "pterodactyl".replace("pterodactyl", "pterodactyl" + str(i))
+    _ = pygame.image.load(IMAGE_PATHS[__])
+    image_pterodactyl.append(_)
+pterodactyls = pygame.sprite.Group()
 
 clock = pygame.time.Clock()
 
@@ -36,11 +61,21 @@ while True:
     if random.randint(0, 100) == 10:
         cloud_sprites_group.add(Cloud(image_cloud, (SCREENSIZE[0], random.randrange(30, 75))))
 
+    if random.randint(0, 100) == 10:
+        cacti.add(Cactus(image_cactus, (SCREENSIZE[0], SCREENSIZE[1])))
+
+    if random.randint(0, 100) == 10:
+        pterodactyls.add(Pterodactyl(image_pterodactyl, (SCREENSIZE[0], random.randrange(20, 75))))
+
     ground.update()
     cloud_sprites_group.update()
+    cacti.update()
+    pterodactyls.update()
 
     ground.draw(screen)
     cloud_sprites_group.draw(screen)
+    cacti.draw(screen)
+    pterodactyls.draw(screen)
 
     pygame.display.update()
     clock.tick(FPS)
